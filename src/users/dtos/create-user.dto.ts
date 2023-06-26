328aca9f4a33a37c16bef52d10ae98d993af46f9
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-06-26 20:41:28 +0700

    fix: validate email and number

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,13 +1,15 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 
 export default class CreateUserDto {
   @IsNotEmpty()
   name: string;
 
   @IsNotEmpty()
+  @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @IsNumber()
   age: number;
 
   @IsNotEmpty()
