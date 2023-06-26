6495c4b3dcdebd499847a131a47a68b913a7bce7
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2023-06-26 20:04:17 +0700

    test: user controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,123 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from './contracts/user.interface';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let svc: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    svc = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should use user service getAll method', async () => {
+    const users = [] as User[];
+    jest.spyOn(svc, 'getAll').mockImplementation(() => Promise.resolve(users));
+
+    expect(await controller.getAll()).toBe(users);
+    expect(svc.getAll).toBeCalled();
+    expect(svc.getAll).toBeCalledTimes(1);
+  });
+
+  it('should use user service getOneById method', async () => {
+    const user = {
+      id: 'id',
+      name: 'name',
+      email: 'email',
+      age: 1,
+      address: 'address',
+    } as User;
+
+    jest
+      .spyOn(svc, 'getOneById')
+      .mockImplementation(() => Promise.resolve(user));
+
+    expect(await controller.getOneById('id')).toBe(user);
+    expect(svc.getOneById).toBeCalled();
+    expect(svc.getOneById).toBeCalledTimes(1);
+  });
+
+  it('should throw not found exception if user id does not exist', async () => {
+    try {
+      await controller.getOneById('dummy id');
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+    }
+  });
+
+  it('should use user service create method', async () => {
+    jest.spyOn(svc, 'create').mockImplementation(() => Promise.resolve(true));
+
+    expect(await controller.create({} as any)).toBeTruthy();
+    expect(svc.create).toBeCalled();
+    expect(svc.create).toBeCalledTimes(1);
+  });
+
+  it('should use user service update method', async () => {
+    const user = {
+      id: 'id',
+      name: 'name',
+      email: 'email',
+      age: 1,
+      address: 'address',
+    } as User;
+
+    jest
+      .spyOn(svc, 'getOneById')
+      .mockImplementation(() => Promise.resolve(user));
+
+    jest.spyOn(svc, 'update').mockImplementation(() => Promise.resolve(true));
+
+    expect(await controller.update('id', {} as any)).toBeTruthy();
+    expect(svc.update).toBeCalled();
+    expect(svc.update).toBeCalledTimes(1);
+  });
+
+  it('should use throw not found if user id to be updated does not exist', async () => {
+    try {
+      await controller.update('dummy id', {} as any);
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+    }
+  });
+
+  it('should use user service delete method', async () => {
+    const user = {
+      id: 'id',
+      name: 'name',
+      email: 'email',
+      age: 1,
+      address: 'address',
+    } as User;
+
+    jest
+      .spyOn(svc, 'getOneById')
+      .mockImplementation(() => Promise.resolve(user));
+
+    jest.spyOn(svc, 'delete').mockImplementation(() => Promise.resolve(true));
+
+    expect(await controller.delete('id')).toBeTruthy();
+    expect(svc.delete).toBeCalled();
+    expect(svc.delete).toBeCalledTimes(1);
+  });
+
+  it('should use throw not found if user id to be deleted does not exist', async () => {
+    try {
+      await controller.delete('dummy id');
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+    }
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,58 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import CreateUserDto from './dtos/create-user.dto';
+import UpdateUserDto from './dtos/update-user.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private svc: UsersService) {}
+
   @Get()
-  findAll() {
-    return 'users';
+  async getAll() {
+    return await this.svc.getAll();
+  }
+
+  @Get(':id')
+  async getOneById(@Param('id') id: string) {
+    const entity = await this.svc.getOneById(id);
+
+    if (!entity) {
+      throw new NotFoundException();
+    }
+
+    return entity;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() payload: CreateUserDto) {
+    return await this.svc.create(payload);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() payload: UpdateUserDto) {
+    const entity = await this.getOneById(id);
+
+    return await this.svc.update({ ...entity, ...payload });
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const entity = await this.getOneById(id);
+
+    return await this.svc.delete(entity);
   }
 }
